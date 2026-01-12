f906b459911c0555e2a2a3cb7d0a8ca37f16200f
Kubatov Syimyk <simik101297@gmail.com>
2026-01-12 19:58:34 +0600

    add logger

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -1,13 +1,19 @@
 package main
 
 import (
+	"log/slog"
+
 	"github.com/kubatov-hub/url-shortener-golang/internal/config"
+	"github.com/kubatov-hub/url-shortener-golang/internal/logger"
 )
 
 func main() {
 	cfg := config.MustLoad()
 
-	// TODO: init logger: slog
+	log := logger.SetupLogger(cfg.Env)
+
+	log.Info("starting url-shortener", slog.String("env", cfg.Env))
+	log.Debug("debug messages are enabled")
 
 	// TODO: init storage: sqlite
 
