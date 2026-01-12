717543f051c375750654abbcd630bd0d79527dbd
Kubatov Syimyk <simik101297@gmail.com>
2026-01-12 22:14:22 +0600

    хз короче чет коонет не срабатывает потом решу пока забарсываю проект лол)

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/kubatov-hub/url-shortener-golang/internal/config"
 	"github.com/kubatov-hub/url-shortener-golang/internal/logger"
+	"github.com/kubatov-hub/url-shortener-golang/internal/storage/sqlite"
 )
 
 func main() {
@@ -16,6 +17,13 @@ func main() {
 	log.Debug("debug messages are enabled")
 
 	// TODO: init storage: sqlite
+	storage, err := sqlite.New(cfg.StoragePath)
+	if err != nil {
+		log.Error("failed to init storage", slog.String("error", err.Error()))
+		return
+	}
+
+	_ = storage
 
 	// TODO: init router: chi, "chi render"
 
